3f6973ac83becae650bd28f56085685ec71ade39
kwentar <kwentar@gmail.com>
2023-05-24 23:43:34 +0300

    #7 section/ about section v.0.2

---
+++ src/utils/consts.js
@@ -0,0 +1,6 @@
+const firstImage = document.getElementById('about-img1');
+const secondImage = document.getElementById('about-img2');
+const thirdImage = document.getElementById('about-img3');
+const firstText = document.getElementById('about-text1');
+const secondText = document.getElementById('about-text2');
+const thirdText = document.getElementById('about-text3');
